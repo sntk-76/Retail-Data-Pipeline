c5ae17457d694c7d78b7309292ac7a3c22480b12
sntk-76 <sn.tvkl@gmail.com>
2025-03-18 11:38:23 +0000

    The infrastructure has been successfully created using the main.tf and variables.tf files, and it has also been tested.

---
+++ Infrastructure/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.17.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file(var.credentials)
+  project     = var.project_id
+  region      = var.region
+}
+
+
+# create a google cloud storage 
+
+resource "google_storage_bucket" "retail_data_bucket" {
+  name = var.bucket_name
+  location = var.region
+  project = var.project_id
+}
+
+# create a google cloud bigquery
+
+resource "google_bigquery_dataset" "retail_dataset" {
+  dataset_id = var.dataset_name
+  location = var.region
+  project = var.project_id
+}
+
+#create a dataproc cluster for spark
+
+resource "google_dataproc_cluster" "spark_cluster" {
+  name   = var.cluster_name
+  region = var.cluster_region
+
+  cluster_config {
+    master_config {
+      num_instances = 1
+      machine_type  = "n1-standard-2"
+      disk_config {
+        boot_disk_size_gb = 40
+      }
+    }
+    worker_config {
+      num_instances = 2
+      machine_type  = "n1-standard-2"
+      disk_config {
+        boot_disk_size_gb = 40
+      }
+    }
+  }
+}
+
---
+++ Infrastructure/variables.tf
@@ -0,0 +1,35 @@
+variable "credentials" {
+    description = "The JSON file for the authhenticaton the service accont"
+    default = "/home/sinatavakoli2022/Retail-Data-Pipeline/Authentication/retail-data-pipeline-453817-5c7165d921e7.json"
+}
+
+variable "project_id" {
+    description = "The id for the project 'retail_data_pipeline'"
+    default = "retail-data-pipeline-453817"
+}
+
+variable "region" {
+    description = "The region of the project"
+    default = "europe-west1"
+}
+
+variable "bucket_name" {
+    description = "The name of the bucket that should be universal"
+    default = "retail-data-pipeline-453817-bucket"
+}
+
+variable "dataset_name" {
+    description = "The name of the  dataset"
+    default = "retail_data_pipeline_dataset"
+}
+
+variable "cluster_name" {
+    description = "The name of the cluster for the pyspark operation"
+    default = "retail-sparl-cluster"
+}
+
+variable "cluster_region" {
+    description = "The valid region for activating the dataproc"
+    default = "europe-west1"
+}
+
