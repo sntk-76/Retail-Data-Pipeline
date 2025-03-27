219cd839861d487e99ac674dc30822b8943671c5
sntk-76 <sn.tvkl@gmail.com>
2025-03-27 10:05:42 +0000

    Two DAG files were created: one for uploading data from the local system to the bucket, and another for transferring the data from the bucket to BigQuery. Additionally, a Jupyter Notebook file was developed to modify the original CSV file, ensuring compatibility with the required data formats. Several queries were executed to validate the data in BigQuery, and all steps were completed successfully without any issues.

---
+++ Airflow/dags/GCS_to_bigquery.py
@@ -0,0 +1,66 @@
+import os
+from airflow import DAG
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from datetime import datetime
+
+default_args = {
+    'owner':'airflow',
+    'start_date':datetime(2024,1,1),
+    'retries':1
+}
+
+with DAG (
+    dag_id = 'bucket_to_bigquery',
+    description='automate the process of the loading data to the big query dataset from the storage bucket',
+    default_args=default_args,
+    catchup=False,
+    schedule_interval=None
+) as dag:
+    
+    transfer_file = GCSToBigQueryOperator(
+
+        task_id = 'load_to_bq',
+        bucket='retail-data-pipeline-453817-bucket',
+        source_objects=['raw/retail_data.csv'],
+        destination_project_dataset_table='retail-data-pipeline-453817.retail_data_pipeline_dataset.retail_raw_data',
+        source_format='CSV',
+        create_disposition='CREATE_IF_NEEDED',
+        write_disposition='WRITE_TRUNCATE',
+        skip_leading_rows=1,
+        autodetect=False,
+        schema_fields=[
+            {"name" : "Transaction_ID", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Customer_ID", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Name", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Email", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Phone", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Address", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "City", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "State", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Zipcode", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Country", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Age", "type": "INTEGER","mode":"NULLABLE"},
+            {"name" : "Gender", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Income", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Customer_Segment", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Date", "type": "DATE","mode":"NULLABLE"},
+            {"name" : "Year", "type": "INTEGER","mode":"NULLABLE"},
+            {"name" : "Month", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Time", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Total_Purchases", "type": "INTEGER","mode":"NULLABLE"},
+            {"name" : "Amount", "type": "FLOAT","mode":"NULLABLE"},
+            {"name" : "Total_Amount", "type": "FLOAT","mode":"NULLABLE"},
+            {"name" : "Product_Category", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Product_Brand", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Product_Type", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Feedback", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Shipping_Method", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Payment_Method", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Order_Status", "type": "STRING","mode":"NULLABLE"},
+            {"name" : "Ratings", "type": "FLOAT","mode":"NULLABLE"},
+            {"name" : "products", "type": "STRING","mode":"NULLABLE"},
+
+        ]
+    )
+
+transfer_file    
